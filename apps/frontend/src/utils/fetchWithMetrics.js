f0a16bb49d80b529f8f173a633208ab105641aaf
ericanoronha <erica.noronha@live.com>
2025-07-30 16:44:27 -0300

    chore: deleta arquivo inutilizado

--- apps/frontend/src/utils/fetchWithMetrics.js
+++
@@ -1,7 +0,0 @@
-export async function fetchWithMetrics(url, options = {}) {
-  const start = performance.now();
-  const res = await fetch(url, options);
-  const duration = performance.now() - start;
-  console.log(`Fetch to ${url} took ${duration.toFixed(2)}ms`);
-  return res;
-}
